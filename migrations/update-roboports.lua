a8d845d5ac6897508387377719e2171e3c771083
nicolas.blexmann <nicolas.blexmann@ext.verallia.com>
2024-01-27 19:57:08 +0200

    fix missing prototype index in migration

--- migrations/update-roboports.lua
+++ migrations/update-roboports.lua
@@ -5,6 +5,7 @@ end
 
 for _,roboport_entity in pairs(game.get_surface(1).find_entities_filtered{type="roboport"}) do
     global.custom_entities[roboport_entity.unit_number]=roboport:new(roboport_entity)
+    global.custom_entities[roboport_entity.unit_number].prototype_index="roboport"
     setmetatable(global.custom_entities[roboport_entity.unit_number],custom_prototypes["roboport"])
     custom_prototypes["roboport"].__index=custom_prototypes["roboport"]
 end
