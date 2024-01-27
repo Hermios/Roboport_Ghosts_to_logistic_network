540809aed82acf0aaed53158f84647d2fb1ad155
nicolas.blexmann <nicolas.blexmann@ext.verallia.com>
2024-01-27 19:44:31 +0200

    check if ghost

--- control.lua
+++ control.lua
@@ -15,6 +15,9 @@ table.insert(list_events.on_built,function(entity)
 end)
 
 table.insert(list_events.on_post_entity_died,function(event)
+    if not event.ghost then
+        return
+    end
     for _,roboport_entity in pairs(game.get_surface(1).find_entities_filtered{type="roboport",position=event.ghost.position,radius=global.max_construction_radius}) do
         if global.custom_entities[roboport_entity.unit_number] then
             global.custom_entities[roboport_entity.unit_number]:update_ghost(event.ghost,true)
