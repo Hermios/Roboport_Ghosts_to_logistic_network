8386754833b00eee87f1e5bc42318088b1d18213
nicolas.blexmann <nicolas.blexmann@ext.verallia.com>
2024-01-27 18:59:40 +0200

    fix entities not loaded

--- prototypes/entities.lua
+++ prototypes/entities.lua
@@ -1,1 +1,4 @@
-createdata("constant-combinator","constant-combinator",roboport_converter_combinator,nil,true)
+createdata("constant-combinator","constant-combinator",roboport_converter_combinator,{
+    circuit_wire_connection_points=data.raw["constant-combinator"]["constant-combinator"].circuit_wire_connection_points,
+    sprites=data.raw["constant-combinator"]["constant-combinator"].sprites
+},true)
