0f25d2575bea4235cd14357768ad3063a79c47dc
Hermios <nicoblex@yahoo.fr>
2024-06-30 20:31:34 +0300

    Prevent self colliding. Add cliff-explosives

--- prototypes/entities.lua
+++ prototypes/entities.lua
@@ -2,3 +2,4 @@ createdata("constant-combinator","constant-combinator",roboport_converter_combin
     circuit_wire_connection_points=data.raw["constant-combinator"]["constant-combinator"].circuit_wire_connection_points,
     sprites=data.raw["constant-combinator"]["constant-combinator"].sprites
 },true)
+data.raw["constant-combinator"][roboport_converter_combinator].collision_mask={"not-colliding-with-itself"}
