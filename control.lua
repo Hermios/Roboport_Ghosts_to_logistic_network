b7372f6eeb9e0e3f859c1267c5f2b5dce8b98a43
Hermios <nicoblex@yahoo.fr>
2024-07-05 13:21:55 +0300

    implement upgrade and landfill ghost

--- control.lua
+++ control.lua
@@ -28,6 +28,14 @@ table.insert(list_events.on_marked_for_deconstruction,function (event)
     end
 end)
 
+table.insert(list_events.on_marked_for_upgrade,function (event)
+    for _,roboport_entity in pairs(game.get_surface(1).find_entities_filtered{type="roboport",position=event.entity.position,radius=global.max_construction_radius}) do
+        if global.custom_entities[roboport_entity.unit_number] then
+            global.custom_entities[roboport_entity.unit_number]:update_all()
+        end
+    end
+end)
+
 table.insert(list_events.on_post_entity_died,function(event)
     if not event.ghost then
         return
